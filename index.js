20d9c609653f46d069b54bd48f2301c4144ca327
rijkvanzanten <rijkvanzanten@me.com>
2018-01-23 18:13:33 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = [
+  'and', 'that', 'but', 'or', 'as', 'if', 'when', 'than', 'because', 'while', 'where',
+  'after', 'so', 'though', 'since', 'until', 'whether', 'before', 'although', 'nor',
+  'like', 'once', 'unless', 'now', 'except'
+];
